0930c397bbee09d329de586ce77186f67a79ea4f
Shivam Pandey <shivamkumarp447@gmail.com>
2025-02-07 14:00:30 +0530

    array

---
+++ array.php
@@ -0,0 +1,20 @@
+<?php
+// check whether an item is present in the cart
+$cart = ["bread","jam","peanut","butter"];
+echo "Total items".count($cart);
+echo "<br>";
+
+foreach($cart as $item){
+    echo "$item";
+    echo "<br>";
+}
+if(in_array("egg",$cart)){
+    echo "Egg is present";
+}
+else{
+    echo "Egg is not present";
+}
+// add Egg
+array_push($cart, "egg");
+print_r($cart);
+?>
